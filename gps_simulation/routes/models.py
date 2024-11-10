1d429ad5f0561947dfee557bb3957c098e0fe2dd
Vero Ramirez <veronica.ramirez@itscordoba.edu.ar>
2024-11-10 12:23:35 -0300

    corregi código reverso de ruta

--- gps_simulation/routes/models.py
+++ gps_simulation/routes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from .models import Route
 
+
 class City(models.Model):
     name = models.CharField(max_length=100, unique=True, blank = False)
     description = models.TextField(null=True)
@@ -11,6 +12,13 @@ class City(models.Model):
     def __str__(self):
         return  f"{self.name} - ({self.description}) - ({self.population} habitantes)"
 
+    def create_inverse_route(self, sender, instance, created, **kwargs):
+        if not Route.objects.filter(start_city=self.end_city, end_city=self).exists():
+            inverse_route = Route.objects.create(
+                start_city=instance.end_city,
+                end_city=instance.start_city,            
+                distance=instance.distance,
+            )
 
 class Route(models.Model):
     start_city = models.ForeignKey(
@@ -23,12 +31,3 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.start_city} -> {self.end_city} ({self.distance} km)"
-
-    def create_inverse_route(sender, instance, created, **kwargs):
-        if created:
-            inverse_route = Route.objects.create(
-            start_city=instance.end_city,
-            end_city=instance.start_city,
-            distance=instance.distance,
-        )
-    post_save.connect(create_inverse_route, sender=Route)
