0ab90287bb368d9f9f1e104e0643a0f7fc1ecda6
Vero Ramirez <veronica.ramirez@itscordoba.edu.ar>
2024-11-10 16:37:43 -0300

    código de ruta reverso funcionando en archivo models

--- gps_simulation/routes/models.py
+++ gps_simulation/routes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
-from .models import Route
+from django.dispatch import receiver
+from django.db.models import UniqueConstraint
 
 
 class City(models.Model):
@@ -12,13 +13,6 @@ class City(models.Model):
     def __str__(self):
         return  f"{self.name} - ({self.description}) - ({self.population} habitantes)"
 
-    def create_inverse_route(self, sender, instance, created, **kwargs):
-        if not Route.objects.filter(start_city=self.end_city, end_city=self).exists():
-            inverse_route = Route.objects.create(
-                start_city=instance.end_city,
-                end_city=instance.start_city,            
-                distance=instance.distance,
-            )
 
 class Route(models.Model):
     start_city = models.ForeignKey(
@@ -31,3 +25,18 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.start_city} -> {self.end_city} ({self.distance} km)"
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['start_city', 'end_city'], name='unique_route')
+        ]
+
+@receiver(post_save, sender=Route)
+def create_inverse_route(sender, instance, created, **kwargs):
+    if created:
+        # Intentar crear la ruta inversa. Si ya existe, se omite.
+        Route.objects.get_or_create(
+            start_city=instance.end_city,
+            end_city=instance.start_city,
+            defaults={'distance': instance.distance}
+        )
